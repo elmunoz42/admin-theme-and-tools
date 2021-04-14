88675fefe3ca5ab71a19bd8dd3ff8c1354ab3682
Carlos Munoz Kampff <carlos+gitkraken@fountain-city.com>
2021-04-14 11:14:36 -0700

    Replace prefix from fc_ to ab_ in aaadmin-boss.php

--- aaadmin-boss.php
+++ aaadmin-boss.php
@@ -47,18 +47,17 @@ function ab_load_the_right_style_when_wp_loaded(){
     } elseif (in_array( 'client', (array) $current_user->roles)){
       add_action('admin_enqueue_scripts', 'ab_client_admin_theme_style');
       add_action('login_enqueue_scripts', 'ab_client_admin_theme_style');
-      // add_action('admin_head', 'cmk_backend_menu_therapist');
     }
   }
 }
 
 
 // Debugging Functions and Alerts
-function fc_alert($output){
+function ab_alert($output){
 	echo "<script type='text/javascript'>alert('" . $output . "');</script>";
 }
 
-function fc_debugout($value, $autodump = false)
+function ab_debugout($value, $autodump = false)
 {
     if ($autodump || (empty($value) && $value !== 0 && $value !== "0") || $value === true) {
         var_dump($value);
@@ -69,7 +68,7 @@ function fc_debugout($value, $autodump = false)
 }
 if (!function_exists('write_log')) {
 
-    function fc_write_log($log) {
+    function ab_write_log($log) {
         if (true === WP_DEBUG) {
             if (is_array($log) || is_object($log)) {
                 error_log(print_r($log, true));
@@ -84,17 +83,17 @@ if (!function_exists('write_log')) {
 
 
 // Creates FC Dev Tools admin bar Tab
-add_action( 'admin_bar_menu', 'fc_add_links_to_admin_bar',999 );
-function fc_add_links_to_admin_bar($admin_bar) {
+add_action( 'admin_bar_menu', 'ab_add_links_to_admin_bar',999 );
+function ab_add_links_to_admin_bar($admin_bar) {
 	// add a parent item
 	$args = array(
-		'id'    => 'fc_dev_tools',
+		'id'    => 'ab_dev_tools',
 		'title' => 'FC Dev Tools',
 		'href'   => 'http://example.com/', // Showing how to add an external link
 	);
 	$admin_bar->add_node( $args );
 	$args = array(
-		'parent' => 'fc_dev_tools',
+		'parent' => 'ab_dev_tools',
 		'id'     => 'media-libray',
 		'title'  => 'Media Library',
 		'href'   => esc_url( admin_url( 'upload.php' ) ),
@@ -103,7 +102,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	$admin_bar->add_node( $args );
 
 	$args = array(
-		'parent' => 'fc_dev_tools',
+		'parent' => 'ab_dev_tools',
 		'id'     => 'plugins',
 		'title'  => 'Plugins',
 		'href'   => esc_url( admin_url( 'plugins.php' ) ),
@@ -112,7 +111,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	$admin_bar->add_node( $args );
 
 	$args = array(
-		'parent' => 'fc_dev_tools',
+		'parent' => 'ab_dev_tools',
 		'id'     => 'sync_db',
 		'title'  => 'Sync Database',
 		'href'   => esc_url( admin_url( 'tools.php?page=wp-migrate-db-pro' ) ),
@@ -128,7 +127,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	if (is_plugin_active('backupbuddy/backupbuddy.php')) {
 			$url = wp_nonce_url(admin_url('plugins.php?action=deactivate&plugin=backupbuddy%2Fbackupbuddy.php&plugin_status=all&paged=1&s&' ), 'activate_bu_buddy', 'my_nonce1');
 	    $args = array(
-	        'parent' => 'fc_dev_tools',
+	        'parent' => 'ab_dev_tools',
 	        'id'     => 'deactivate_bu_buddy',
 	        'title'  => 'Deactivate BU Buddy',
 	        'href'   => esc_url( (wp_nonce_url(admin_url('plugins.php?action=deactivate&plugin=backupbuddy%2Fbackupbuddy.php&plugin_status=all&paged=1&s'), 'action') ) ),
@@ -138,7 +137,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	} else {
 		$url = wp_nonce_url(admin_url('plugins.php?action=activate&plugin=backupbuddy%2Fbackupbuddy.php&plugin_status=all&paged=1&s&' ), 'deactivate_bu_buddy', 'my_nonce2');
 		$args = array(
-				'parent' => 'fc_dev_tools',
+				'parent' => 'ab_dev_tools',
 				'id'     => 'activate_bu_buddy',
 				'title'  => 'Activate BU Buddy ',
 				'href'   => esc_url( $url  ) ,
@@ -151,7 +150,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	// if (is_plugin_active('wp-migrate-db-pro/wp-migrate-db-pro.php')) {
 	//
 	//     $args = array(
-	//         'parent' => 'fc_dev_tools',
+	//         'parent' => 'ab_dev_tools',
 	//         'id'     => 'deactivate_wp-migrate-db-pro',
 	//         'title'  => 'Deactivate WP Migrate DB Pro',
 	//         'href'   => esc_url( admin_url( 'plugins.php?action=deactivate&plugin=wp-migrate-db-pro%2Fwp-migrate-db-pro.php&plugin_status=all&paged=1&s&_wpnonce=' . esc_attr( $nonce ) ) ),
@@ -160,7 +159,7 @@ function fc_add_links_to_admin_bar($admin_bar) {
 	// 		$admin_bar->add_node( $args );
 	// } else {
 	// 	$args = array(
-	// 			'parent' => 'fc_dev_tools',
+	// 			'parent' => 'ab_dev_tools',
 	// 			'id'     => 'activate_wp-migrate-db-pro',
 	// 			'title'  => 'Activate WP Migrate DB Pro',
 	// 			'href'   => esc_url( admin_url( 'plugins.php?action=activate&plugin=wp-migrate-db-pro%2Fwp-migrate-db-pro.php&plugin_status=all&paged=1&s&_wpnonce=' . esc_attr( $nonce )  ) ),
@@ -174,14 +173,14 @@ function fc_add_links_to_admin_bar($admin_bar) {
 
 // Creates Dashboard Widget
 // NOTE: Add additional links as desired
-add_action('wp_dashboard_setup', 'fc_dashboard_widgets');
-function fc_dashboard_widgets() {
+add_action('wp_dashboard_setup', 'ab_dashboard_widgets');
+function ab_dashboard_widgets() {
 	global $wp_meta_boxes;
 
-	wp_add_dashboard_widget('fc_dev_dashboard', 'Fountain City Support and Resources ', 'fc_dev_dashboard');
+	wp_add_dashboard_widget('ab_dev_dashboard', 'Fountain City Support and Resources ', 'ab_dev_dashboard');
 }
 
-function fc_dev_dashboard() {
+function ab_dev_dashboard() {
 	echo '<p>If you need any support please use our ticketing system, it helps us stay organized :-)</p>';
 	echo '<ul><li><a href="https://tickets.fountain-city.com/" target="_blank">Create a Support Ticket</a></li>';
 	echo '<ul><li><a href="https://fountaincity.host" target="_blank">Manage your Hosting Subscription</a></li>';
@@ -191,24 +190,24 @@ function fc_dev_dashboard() {
 
 
 // Auto activate plugins
-add_action('init','fc_activate_development_plugins');
-function fc_activate_development_plugins(){
+add_action('init','ab_activate_development_plugins');
+function ab_activate_development_plugins(){
 
 	$current_user = wp_get_current_user();
 
-	// set_transient( 'fc_dev_is_logged_in' , 0 , HOUR_IN_SECONDS ); // NOTE TEMPORARY FOR TESTING
-	$fc_dev_is_logged_in = (get_transient('fc_dev_is_logged_in')) ? 1 : 0;
+	// set_transient( 'ab_dev_is_logged_in' , 0 , HOUR_IN_SECONDS ); // NOTE TEMPORARY FOR TESTING
+	$ab_dev_is_logged_in = (get_transient('ab_dev_is_logged_in')) ? 1 : 0;
 
 	$backup_buddy = ABSPATH."wp-content/plugins/backupbuddy/backupbuddy.php";
 	$wp_migrate_db_pro = ABSPATH."wp-content/plugins/wp-migrate-db-pro/wp-migrate-db-pro.php";
 	$query_monitor = ABSPATH."wp-content/plugins/query-monitor/query-monitor.php";
-	// If the current user is an administrator, and the fc_dev_is_logged_in transient hasn't yet been set to true
-	if (current_user_can('administrator') && $fc_dev_is_logged_in == 0){
+	// If the current user is an administrator, and the ab_dev_is_logged_in transient hasn't yet been set to true
+	if (current_user_can('administrator') && $ab_dev_is_logged_in == 0){
 		// If one of our devs is the current user
 		if ($current_user->user_login == 'sebastian' || $current_user->user_login == 'will' || $current_user->user_login == 'carlos' || $current_user->user_login == 'carlos.mk' || $current_user->user_login == 'gray') {
 
-			set_transient( 'fc_dev_is_logged_in' , 1 , HOUR_IN_SECONDS );
-			$fc_dev_is_logged_in == 1;  // Not sure why I had to do this. Plugins were deactivating on refresh when I didn't ??
+			set_transient( 'ab_dev_is_logged_in' , 1 , HOUR_IN_SECONDS );
+			$ab_dev_is_logged_in == 1;  // Not sure why I had to do this. Plugins were deactivating on refresh when I didn't ??
 			$message = 'The below development plugin(s) have been activated by fc dev tools:\n';
 			if (!file_exists($backup_buddy)&&!file_exists ($wp_migrate_db_pro)&&!file_exists ($query_monitor)) {
 				$message = 'BackupBuddy, Wp Migrate DB Pro and Query Monitor are not installed.';
@@ -226,32 +225,32 @@ function fc_activate_development_plugins(){
 				activate_plugin( $query_monitor );
 				$message .= 'Query Monitor\n';
 			}
-			fc_alert($message);
+			ab_alert($message);
 		}
 	}
 }
 
 // NOTE troubleshooting
-// fc_alert(get_transient('fc_dev_is_logged_in'));
+// ab_alert(get_transient('ab_dev_is_logged_in'));
 
 // Auto deactivate plugins
-add_action('init','fc_deactivate_development_plugins');
-function fc_deactivate_development_plugins(){
+add_action('init','ab_deactivate_development_plugins');
+function ab_deactivate_development_plugins(){
 	$current_user = wp_get_current_user();
 
-	// set_transient( 'fc_dev_is_logged_in' , 1 , HOUR_IN_SECONDS ); // NOTE TEMPORARY FOR TESTING
-	$fc_dev_is_logged_in = (get_transient('fc_dev_is_logged_in')) ? 1 : 0;
-	// fc_alert($fc_dev_is_logged_in);
+	// set_transient( 'ab_dev_is_logged_in' , 1 , HOUR_IN_SECONDS ); // NOTE TEMPORARY FOR TESTING
+	$ab_dev_is_logged_in = (get_transient('ab_dev_is_logged_in')) ? 1 : 0;
+	// ab_alert($ab_dev_is_logged_in);
 
-	// If the fc_dev_is_logged_in transient is still 1 ...
-	if($fc_dev_is_logged_in == 1){
+	// If the ab_dev_is_logged_in transient is still 1 ...
+	if($ab_dev_is_logged_in == 1){
 		// ... and neither Sebastian, Will, Carlos or Gray are logged in, deactivate the plugins. This should only be true for an instant.
 		if ($current_user->user_login != 'sebastian' && $current_user->user_login != 'will' && $current_user->user_login != 'carlos' && $current_user->user_login != 'carlos.mk' && $current_user->user_login != 'gray'){
 			$backup_buddy = ABSPATH."wp-content/plugins/backupbuddy/backupbuddy.php";
 			$wp_migrate_db_pro = ABSPATH."wp-content/plugins/wp-migrate-db-pro/wp-migrate-db-pro.php";
 			$query_monitor = ABSPATH."wp-content/plugins/query-monitor/query-monitor.php";
 
-			set_transient( 'fc_dev_is_logged_in' , 0 , HOUR_IN_SECONDS );
+			set_transient( 'ab_dev_is_logged_in' , 0 , HOUR_IN_SECONDS );
 			if (file_exists ($backup_buddy)) {
 				deactivate_plugins( $backup_buddy );
 			}
@@ -261,7 +260,7 @@ function fc_deactivate_development_plugins(){
 			if (file_exists ($query_monitor)) {
 				deactivate_plugins( $query_monitor );
 			}
-			fc_alert('plugins_deactivated');
+			ab_alert('plugins_deactivated');
 		}
 	}
 }
