442351fffea00c1bf71422049e757d1a12a4dd6c
Carlos Munoz Kampff <elmunoz42@gmail.com>
2021-04-19 14:31:12 -0700

    create dashboard-pag-alt to try to debug issue.

--- admin/dashboard-page-alt.php
+++ admin/dashboard-page-alt.php
@@ -17,9 +17,7 @@ function ab_display_dashboard_page() {
 	// check if user is allowed access
 	if ( ! current_user_can( 'manage_options' ) ) return;
 
-	// Get dashboard url
-	$options = get_option( 'ab_options', ab_options_default() );
-	$value = isset( $options['dashboard_url'] ) ? sanitize_text_field( $options['dashboard_url'] ) : '';
+
 	?>
 
 	<div class="wrap">
@@ -30,14 +28,14 @@ function ab_display_dashboard_page() {
 			// output security fields
 			settings_fields( 'ab_options' );
 
-      // NOTE: This was working before.... *************************************
-			// var_dump($options);
-			echo '<iframe width="1200" height="5000" src="' . $value . '" frameborder="0" style="border:0" allowfullscreen></iframe>';
 			// output setting sections
-			do_settings_sections( 'aaadmin-boss' );
+
+			// NOTE: Not working permissions issue **********************************************************************************
+			do_settings_sections( 'dashboard-page' );
+			// do_settings_sections( 'aaadmin-boss' );
 
 			// submit button
-			submit_button();
+			// submit_button();
 
 			?>
 
